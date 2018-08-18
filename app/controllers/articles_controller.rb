7f8d86ab7a3a854059d4da6afb8fc3ce2c6a4ae2
wichru <wicher.maciej@gmail.com>
2018-08-18 15:59:55 +0200

    create articles create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,17 @@
 class ArticlesController < ApplicationController
   def index
-    
+
+  end
+
+  def new
+  end
+
+  def create
+    article_params = params.require(:article).permit(:title, :text)
+
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new', as: 'new_article'
+  post 'articles', to: 'articles#create'
 end
