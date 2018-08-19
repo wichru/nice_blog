d76944c3d126a3a04ea2b9015a4bd7911a583c58
wichru <wicher.maciej@gmail.com>
2018-08-19 11:55:57 +0200

    create update for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,4 +22,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article_params = params.require(:article).permit(:title, :text)
+
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new', as: 'new_article'
   get 'articles/:id', to: 'articles#show', as: 'article'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
 end
