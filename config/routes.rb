731b4f627d283a4977e2fb71add1098b7713c053
wichru <wicher.maciej@gmail.com>
2018-08-19 12:04:03 +0200

    create destroy action for article

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   post 'articles', to: 'articles#create'
   patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
