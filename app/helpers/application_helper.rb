78076fa557f7a9b4ee3b1be1735113351871ff04
wichru <wicher.maciej@gmail.com>
2018-09-01 14:50:52 +0200

    create an email validator and use for commenter in comment model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def resource_name(resource)
     resource.class.name.underscore.humanize.downcase
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
-class Comment < ApplicationRecord
-  validates :body, presence: true, length: { maximum: 500, minimum: 5 }
-  validates :commenter, presence: true
+# frozen_string_literal: true
 
+class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :commenter, presence: true, email: true
+  validates :body, presence: true, length: { maximum: 500, minimum: 5 }
 end
