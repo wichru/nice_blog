234b24f37a96d0659e75271ea604b38718d43c61
wichru <wicher.maciej@gmail.com>
2018-09-09 11:26:01 +0200

    prevent to edit other user article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@
 
 class ArticlesController < ApplicationController
   before_action :provide_article, only: %i[show edit update destroy]
+  before_action :check_article_owner, only: %i[edit update]
 
   def index
     @articles = Article.all.order(created_at: :desc)
@@ -44,6 +45,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def check_article_owner
+    redirect_to articles_path if @article.user != current_user
+  end
 
   def provide_article
     @article = Article.find(params[:id])
